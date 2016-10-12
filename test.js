e2a7ba0b896ff43e0a3b655f305606267e79eee9
Colin Vinson <cvinson86@gmail.com>
2016-10-12 09:46:44 -0400

    Added spec for router

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
+var _ = require('lodash');
+var restify = require('restify');
 var test = require('tape');
 var SearchkitRestify = require('./');
 
 test('adds endpoint to routes', function(assert) {
+  var server = restify.createServer();
+  SearchkitRestify({ index: 'anIndex' }, server);
+
+  var searchRoute = _.filter(server.router.routes.POST, function(route) {
+    return route.spec.path === '/_search';
+  });
+
   assert.plan(1);
-  assert.equal(true, true);
+  assert.equal(searchRoute.length, 1, 'routes list contains /_search');
 });
