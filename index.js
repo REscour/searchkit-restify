32755abaa76ea884f0e6146fcd9ffcb236b3d637
Colin Vinson <cvinson86@gmail.com>
2016-10-11 22:59:25 -0400

    Removed debug dependency, added middleware and useless test

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 var _ = require('lodash');
 var restify = require('restify');
-var debug = require('debug')('SearchkitRestify');
 var request = require('request');
 
-var searchkitRestify = function(config, server) {
+module.exports = function(config, server) {
+  config.queryProcessor = config.queryProcessor || _.identity
 
+  var requestClient = request.defaults({
+    pool: {
+      maxSockets: config.maxSockets || 1000
+    }
+  });
+
+  var elasticRequest = function(url, body) {
+    var fullUrl = config.host+ "/" + config.index + url;
+
+    return requestClient.post({
+      url: fullUrl,
+      body: body,
+      json: _.isObject(body),
+      forever: true
+    });
+  }
+
+  server.post('/_search', function(req, res) {
+    var queryBody = config.queryProcessor(req.body || {}, req, res);
+    elasticRequest('/_search', queryBody).pipe(res);
+  });
 }
-
-module.exports = searchkitRestify;
--- test.js
+++ test.js
@@ -1,1 +1,7 @@
 var test = require('tape');
+var SearchkitRestify = require('./');
+
+test('adds endpoint to routes', function(assert) {
+  assert.plan(1);
+  assert.equal(true, true);
+});
