a87967bdae0fc4d28afefa503f4e960fa7fdde42
Colin Vinson <cvinson86@gmail.com>
2016-10-11 22:28:36 -0400

    Added lodash, debug, and request dependencies

---
+++ index.js
@@ -0,0 +1,10 @@
+var _ = require('lodash');
+var restify = require('restify');
+var debug = require('debug')('SearchkitRestify');
+var request = require('request');
+
+var searchkitRestify = function(config, server) {
+
+}
+
+module.exports = searchkitRestify;
